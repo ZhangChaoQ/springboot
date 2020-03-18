8f17b5dcefa7f37f823e683e8fac3b40777166dd
zhang <1403525893@qq.com>
2020-03-18 18:43:33 +0800

    Initial commit

---
+++ src/main/java/com/ll/springboot/UserRealm.java
@@ -0,0 +1,22 @@
+package com.ll.springboot;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class UserRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+// 这里做权限控制
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+// 这里做登录控制
+        return null;
+    }
+}
